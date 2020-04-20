6880c951f0e82aba7ae74d4dc7fbd251246b1e2d
sunzl <v_sunzilian@baidu.com>
2020-04-20 18:29:52 +0800

    add element-ui

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import api from '@/api';
+import ElementUI from 'element-ui'
+import './styles.scss'
+
+Vue.use(ElementUI)
 
 Vue.prototype.$api = api;
 
